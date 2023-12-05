6eef41871f0d37344e5b53a59a6aaf04aecafe2a
Sanchit Poudel <snchtpdl12@gmail.com>
2023-12-05 22:03:14 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './Components/NavBar'
+
+const App = () => {
+  return (
+    <>
+    <NavBar/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className=' text-black  bg-bg-light dark:bg-navbar-bg pt-3 pb-3 top-0 sticky z-99999 shadow-2xl z-50 font-Roboto' >
+       <div>BenchWarmers Nepal</div> 
+
+    </div>
+    )
+}
+
+export default NavBar
