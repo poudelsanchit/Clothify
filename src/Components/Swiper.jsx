a2b8781966f33cf1db39e058550bc93570f4c78d
“Nabin <nabinkandel@student.pu.edu.np>
2023-12-06 23:12:14 +0545

    feat: :sparkles:

--- src/Components/Swiper.jsx
+++ src/Components/Swiper.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import image1 from '../assets/image1.png'
+// import image2 from '../assets/image2'
+// import image3 from '../assets/image3'
+// import image4 from '../assets/image4'
 
 function Swiper() {
   return (
-    <div><img className=" h-[40rem] object-cover  right-1  " src="public/hello.jpeg" alt="hero-image" /></div>
+    <div><img className=" h-[40rem] object-cover  right-1  " src={image1} alt="hero-image" />
+    
+    
+    </div>
   )
 }
 
