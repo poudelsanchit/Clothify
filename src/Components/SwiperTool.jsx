d0be251fe4eca4f99a7b1c1a6db10d2c039181eb
“Nabin <nabinkandel@student.pu.edu.np>
2023-12-07 00:13:45 +0545

    update Swiper.jsx and Home.jsx

---
+++ src/Components/SwiperTool.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import image1 from '../assets/image1.png';
+import image2 from '../assets/image2.jpeg';
+import image3 from '../assets/image3.png';
+import image4 from '../assets/image4.png';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+function SwiperTool() {
+  return (
+    <>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        className=" h-[40rem] w-[29rem]  object-cover "
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay={{ delay: 1000, disableOnInteraction: false }}
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide>
+          <img className=" h-[40rem]  object-cover " src={image4} alt="hero-image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className=" h-[40rem] object-cover" src={image2} alt="hero-image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className=" h-[40rem] object-cover" src={image3} alt="hero-image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className=" h-[40rem] object-cover" src={image1} alt="hero-image" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
+
+export default SwiperTool;
