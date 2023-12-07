9c978d31bc8a4fe955600251a6595d22a6b9614a
Sanchit Poudel <snchtpdl12@gmail.com>
2023-12-07 16:38:01 +0545

    refactor: :art:

--- src/Components/SwiperTool.jsx
+++ src/Components/SwiperTool.jsx
@@ -11,31 +11,32 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 function SwiperTool() {
+  const SwiperImages=[image1,image2,image3,image4];
   return (
     <>
       <Swiper
         // install Swiper modules
-        modules={[Autoplay,Navigation, Pagination, Scrollbar, A11y]}
+        modules={[Autoplay, Navigation, Pagination, Scrollbar, A11y]}
         className="h-[40rem] w-[29rem] object-cover"
         spaceBetween={0}
         slidesPerView={1}
         autoplay={{
           delay: 1000,
           disableOnInteraction: false,
-        }}        pagination={{ clickable: true }}
+        }}
+        pagination={{ clickable: true }}
       >
-        <SwiperSlide>
-          <img className="h-[40rem] object-cover" src={image4} alt="hero-image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="h-[40rem] object-cover" src={image2} alt="hero-image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="h-[40rem] object-cover" src={image3} alt="hero-image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="h-[40rem] object-cover" src={image1} alt="hero-image" />
-        </SwiperSlide>
+        {SwiperImages.map((images) => {
+          return (
+            <SwiperSlide>
+              <img
+                className="h-[40rem] object-cover"
+                src={images}
+                alt="hero-image"
+              />
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </>
   );
