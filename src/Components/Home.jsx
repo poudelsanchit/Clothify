61194008cf4ec7900a888f8e9514efb8e8471e50
“Nabin <nabinkandel@student.pu.edu.np>
2023-12-07 20:32:12 +0545

    update Home.jsx, HomeShop.jsx and src/index.css

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,7 +6,7 @@ import Swiper from './SwiperTool.jsx';
 
 function Home() {
   return (
-    <>
+  
       <div className="flex w-full   pl-12 justify-between ">
         <div className=" w-3/6 mt-8">
           <div className="font-Poppins font-black text-8xl  mb-4 ">
@@ -26,6 +26,7 @@ function Home() {
           </p>
 
           <Stack direction="row" spacing={4} align="center">
+            <a href="#homeShop">
             <Button
               className="flex gap-1 p-2 mt-6 text-4xl"
               variant="solid"
@@ -37,13 +38,13 @@ function Home() {
               height={12}
             >
               Shop <TbShoppingBag className="text-xl" />
-            </Button>
+            </Button> </a>
           </Stack>
         </div>
 
         <Swiper className=" h-[40rem] bg-pink-100" />
       </div>
-    </>
+
   );
 }
 
