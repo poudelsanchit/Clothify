e9c4e7ff8559d26bb12b45eb378c3f1b6b914176
Sanchit poudel <snchtpdl12@gmail.com>
2023-12-08 20:48:45 +0545

    update 5 files and delete 1 file

--- src/Components/Swiper.jsx
+++ src/Components/Swiper.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
 
-import image1 from '../assets/image1.png'
-// import image2 from '../assets/image2'
-// import image3 from '../assets/image3'
-// import image4 from '../assets/image4'
-
-function Swiper2() {
+const Swiper = () => {
   return (
-    <div>
-      <img className=" h-[40rem] object-cover  right-1  " src={image1} alt="hero-image" />
-    
-    
-    </div>
+    <div>Swiper</div>
   )
 }
 
-export default Swiper2
+export default Swiper
