e938067bed629c38057aa0ca259783b0d1bf077b
Sanchit poudel <snchtpdl12@gmail.com>
2023-12-11 20:00:12 +0545

    update App.jsx, Billing.jsx, EachCard.jsx and Shipping.jsx

--- src/Components/Payments/Shipping/EachCard.jsx
+++ src/Components/Payments/Shipping/EachCard.jsx
@@ -7,7 +7,7 @@ const EachCard = () => {
       setQty(newValue)
     };
   return (
-    <div className=" flex h-32 w-8/12 border-2 rounded-md  p-1 gap-5">
+    <div className=" flex h-32 w-full border-2 rounded-md  p-1 gap-5">
           <img
             src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/21692e08-697b-4bda-9b9d-fea249a11439/air-jordan-1-high-og-royal-reimagined-mens-shoes-gX9FS0.png"
             alt=""
--- src/Components/Payments/Shipping/Shipping.jsx
+++ src/Components/Payments/Shipping/Shipping.jsx
@@ -1,22 +1,54 @@
+import { Button, IconButton, Stack } from '@chakra-ui/react';
 import EachCard from './EachCard';
+import { CiHeart, CiShoppingCart } from 'react-icons/ci';
 
 const Shipping = () => {
  
 
 
   return (
-    <div className=" w-11/12 font-Poppins flex flex-col gap-5">
-      <div className="text-2xl font-medium">Your Orders</div>
-      <div className="flex flex-col gap-2">
-        <EachCard/>
-        <EachCard/>
-        
-      
-        
-        
-        <div className="h-28 w-4/12 "></div>
+    <>
+      <div className="flex w-11/12 gap-10 ">
+        <div className=" w-8/12 font-Poppins flex flex-col gap-5">
+          <div className="text-2xl font-medium">Your Orders</div>
+          <div className="flex flex-col gap-2 w-full">
+            <EachCard />
+            <EachCard />
+          </div>
+        </div>
+        <div className="h-max w-4/12 pb-5  mt-12  rounded-md border-2 hover:shadow-xl transition-all flex flex-col pl-5 pr-5">
+          <div className="font-semibold text-xl font-Poppins pb-2 border-b-2 pt-2">
+            Order Summary
+          </div>
+          <div className=" text-xl font-Poppins flex pb-2">
+            {" "}
+            <div>Subtotal:</div> <div className="ml-auto ">$ 149</div>{" "}
+          </div>
+          <div className=" text-xl font-Poppins flex pb-2">
+            {" "}
+            <div>Shipping Fee:</div> <div className="ml-auto "> $ 2</div>{" "}
+          </div>
+          <div className=" text-xl font-Poppins flex pb-2">
+            {" "}
+            <div>Total :</div> <div className="ml-auto  text-red-600 font-semibold"> $ 151</div>{" "}
+            
+          </div>
+              <Button
+                className="flex gap-3  mt-10 text-4xl "
+                variant="solid"
+                bgColor="black"
+                color="white"
+                colorScheme="#ffffff"
+                height={12}
+                _hover={{ bgColor: "#4F5054", color: "white" }} onClick={()=>dispatch(addItemToCart(product))}
+              >
+                <CiShoppingCart  className='text-2xl'/>
+                Proceed to checkout
+              </Button>
+            
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
