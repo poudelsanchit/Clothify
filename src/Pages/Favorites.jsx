2f5670fb34fcfe0b4a822e7f1095cba5f7d74a12
Sanchit poudel <snchtpdl12@gmail.com>
2023-12-30 23:37:47 +0545

    update 14 files

--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 const Favorites = () => {
   const favorites = useSelector((state)=>state.favorites.favorites)
-  console.log(favorites)
   return (
     <div className=" h-auto min-h-screen  w-full flex justify-center">
       <div className="w-11/12">
-        <div className="lg:text-3xl text-xl font-medium tracking-tighter py-2">
+        <div className="lg:text-3xl text-xl font-medium tracking-tighter py-2 font-Poppins">
           Favorites
         </div>
         <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-5">
           {favorites.map((items) => {
-            return <div className="h-80 bg-purple-600 rounded-md">
-              <img src={items.image} className='h-5/6 w-full object-cover rounded-t-md'  alt="" />
-            </div>;
+            return (
+              <Link to={`/shoes/${items.productId}`}>
+              <div className="h-96  rounded-md flex flex-col gap-1 cursor-pointer">
+                <img
+                  src={items.image}
+                  className="h-5/6 w-full object-cover rounded-t-md"
+                  alt=""
+                />
+                <div className="font-Poppins text-xl font-medium flex w-full">
+                  <div> {items.name}</div>
+                
+                </div>
+                <div className="font-Poppins text-base text-secondary-text">
+                  {items.price}
+                </div>
+              </div>
+              </Link>
+            );
           })}
 
           {/* Updated width to w-1/5 for the fifth card */}
