c2f4e29ba2ea0d6c3582cde829addc8d668f7123
Sanchit poudel <snchtpdl12@gmail.com>
2024-01-01 11:45:42 +0545

    update Nissbody.jsx, Offer.jsx and Men.jsx

--- src/Components/NisseProduct/Nissbody.jsx
+++ src/Components/NisseProduct/Nissbody.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Nissbody() {
+function Nissbody({title,imgurl,subtitle1,subtitle2}) {
   return (
     <div>
       <div className=" mx-auto w-11/12 flex flex-col  justify-center ">
         <div className=" flex items-center gap-96 my-6">
-          <div className="text-3xl ">Men</div>
+          <div className="text-3xl ">{title}</div>
           <div className=" flex gap-20 text-base  font-medium cursor-pointer">
             {/* <div>Shoes</div>
             <div>Clothing</div>
@@ -15,7 +15,7 @@ function Nissbody() {
         </div>
         <div>
           <img
-            src="https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_1824,c_limit/bf6ccafa-64c3-4c0f-b6e2-084f4e27af2f/men-s-shoes-clothing-accessories.jpg"
+            src={imgurl}
             alt="Nike Just Do It"
           />
         </div>
@@ -23,11 +23,10 @@ function Nissbody() {
         {/* Heading */}
         <div className="  ">
           <div className=" flex text-center mx-auto mt-12 mb-8  w-1/2 font-extrabold text-7xl">
-            THE RESOLUTION STARTER PACK
+            {subtitle1}
           </div>
           <div className=" flex text-center mx-auto my-8  w-1/2  text-base">
-            Get after your fitness goals with a curated collection of comfy
-            kicks,lightweight tops, and more supportive styles.
+            {subtitle2}
           </div>
           <a href="#homeShop">
             <div className="flex text-center font-semibold mx-auto my-6 bg-black text-white rounded-full w-fit   px-6 py-2 text-base cursor-pointer">
