587a33cc3f26f6875a61d9883dabf05ebd432bf1
Sanchit poudel <snchtpdl12@gmail.com>
2024-04-05 02:55:57 +0545

    refactor: :art:

--- frontend/src/Components/Payments/Cart/EachCart.jsx
+++ frontend/src/Components/Payments/Cart/EachCart.jsx
@@ -15,6 +15,7 @@ import {
   SwiperImage4,
 } from "../../../assets/index";
 import { CiTrash, CiShoppingCart } from "react-icons/ci";
+import { Button } from "@chakra-ui/react";
 
 const EachCard = ({ items }) => {
   const dispatch = useDispatch();
@@ -57,11 +58,14 @@ const EachCard = ({ items }) => {
         className="w-28 h-28 m-1  rounded-md object-cover "
       />
 
-      <div className="flex flex-col m-1  justify-evenly w-full">
-        <div className="font-bold sm:text-xl text-sm">{items.name}</div>
+      <div className="flex flex-col m-1  justify-evenly w-full ">
+        <div className="font-bold sm:text-xl text-sm">
+          <div>{items.name}</div>
+          <div className="text-base text-red-500">${totalPrice}</div>
+        </div>
 
-        <div className="  text-base flex sm:flex-row flex-col justify-start  sm:gap-10 gap-2 text-secondary-text pt-2">
-          <div className='flex gap-2 sm:gap-5  sm:flex-row flex-col '>
+        <div className="  text-base flex sm:flex-row flex-col justify-start  sm:gap-10 gap-2 text-secondary-text pt-2 ">
+          <div className='flex gap-2 sm:gap-5  sm:flex-row flex-col  '>
           <div className="flex items-center ">
             <span className="font-medium sm:text-base text-xs w-10">Qty: </span>
             <GetNumberRange
@@ -98,14 +102,14 @@ const EachCard = ({ items }) => {
             </select>
           </div>
         </div>
-      </div>
-
-      <div className='flex h-full justify-center items-center cursor-pointer text-3xl' onClick={()=>dispatch(removeItemFromCart(items._id))}><CiTrash/></div>
-      <div className="hidden lg:flex justify-center items-center h-full ml-auto font-Poppins font-medium  text-lg w-20  text-black rounded-r-md ">
-        {" "}
-        ${totalPrice}
+      
       </div>
     </div>
+    <div className="flex flex-col justify-center items-center"> 
+        <div className='flex p-5  h-full  justify-center items-center cursor-pointer text-3xl bg-black rounded-r-md text-white' onClick={()=>dispatch(removeItemFromCart(items._id))}><CiTrash/></div>
+        
+        </div>
+       
     </div>
   );
 };
